61b7ca18eb4874c47c8064555be7f11a8d22106c
Magdalena Nozdrzykowska <m.wrobel@am.szczecin.pl>
2022-12-19 22:45:57 +0100

    dodanie using namespace std

--- ConsoleApplication7/ConsoleApplication7.cpp
+++ ConsoleApplication7/ConsoleApplication7.cpp
@@ -3,9 +3,10 @@
 
 #include <iostream>
 
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << "Hello World!\n";
 }
 
 // Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
